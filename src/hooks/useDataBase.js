2b238e5483c9cc1e0af3b2f13c4cba5178ef33c0
hebirong <hebirong@igengmei.com>
2022-08-25 15:34:33 +0800

    fix indexDB get network

--- src/hooks/useDataBase.js
+++ src/hooks/useDataBase.js
@@ -2,7 +2,7 @@ import * as zango from "zangodb"
 import { getLocal } from '../utils'
 export default function useDataBase() {
   const setDataBase = async() => {
-    const currNetwork = getLocal('currentNetwork')
+    const currNetwork = getLocal('network')
     const dbname = 'chats-'+ (currNetwork ? currNetwork : '') + '-' + getLocal('account')
     let db = new zango.Db(dbname, 2, {chatInfos:['id', 'room']})
     return db
