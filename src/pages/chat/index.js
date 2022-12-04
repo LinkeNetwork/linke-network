dd9305bd9fef0bb3ca22ac74606bdf056f8b0921
alicelite <hebirong@igengmei.com>
2022-12-04 13:12:50 +0800

    fix quit room

--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -303,7 +303,11 @@ export default function Chat() {
       console.log(e, 'err====')
       setShowMask(false)
       if (!hasNotice) {
-        alert('This is not a chat room.')
+        if(getLocal('isConnect')) {
+          alert('This is not a chat room.')
+        } else {
+          alert('Please connect wallet first')
+        }
         setHasNotice(true)
         setShowMask(false)
       }
