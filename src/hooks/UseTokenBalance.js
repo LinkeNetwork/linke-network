4eabc8c80f8ab01be63129c3f5589b07258c6b52
Bretts <ImBrett@proton.me>
2022-12-05 21:42:47 +0800

    update allowanceAction

--- src/hooks/UseTokenBalance.js
+++ src/hooks/UseTokenBalance.js
@@ -1,8 +1,12 @@
 import Web3 from 'web3'
 import BigNumber from 'bignumber.js'
 import { useState } from 'react'
-import { getBalanceNumber, getLocal, getBalance } from '../utils'
+import useGlobal from './useGlobal'
+import { ethers } from "ethers"
+import { getBalanceNumber, getLocal, getBalance, allowance } from '../utils'
 export default function UseTokenBalance() {
+  const { accounts } = useGlobal()
+  const [authorization, setAuthorization] = useState(false)
   const [poolBalance,setPoolBalance] = useState(0)
   const [hasGetTokenList, setHasGetTokenList] = useState(false)
   const [tokenList, setTokenList] = useState([])
@@ -23,5 +27,19 @@ export default function UseTokenBalance() {
       }
     }
   }
-  return { poolBalance, tokenList, getTokenBalance }
+  const allowanceAction = async (from) => {
+    let account = accounts || localStorage.getItem('account')
+    const {provider, currency, router: spender, tokenValue} = from
+    const {tokenAddress} = currency
+    const allowanceTotal = await allowance({provider, tokenAddress, spender, account})
+    const amountToken = ethers.utils.parseEther(tokenValue)
+    const allonceNum = ethers.utils.parseEther(allowanceTotal)
+    console.log("allowanceAction", amountToken, allonceNum, allonceNum.gte(amountToken))
+    return allonceNum.gte(amountToken)
+  }
+  const getAuthorization = async(from) => {
+    const allowanceResult = from.currency.tokenAddress ? await allowanceAction(from) : true
+    setAuthorization(allowanceResult)
+  }
+  return { poolBalance, tokenList, getTokenBalance, authorization }
 }
